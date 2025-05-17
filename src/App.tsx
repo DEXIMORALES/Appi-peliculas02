e1c2c7a29068344840093edb42e2df2f26f56754
Dexi <dexiblue.desing@gmail.com>
2025-05-17 15:51:49 -0500

    update pages

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// Make sure the file exists at src/pages/Login.tsx or src/pages/Login.jsx
+// Make sure the file exists at src/pages/Login.tsx or src/pages/Login.jsx
+// If the file is named differently or in another folder, update the path accordingly.
+import {Login} from "./assets/pages/login/index";
+const App = () => {
+  return (
+   <>
+   <Login />
+   </>
+  
+  );  
+ };
+
+export default App;
---
+++ src/assets/pages/login/index.tsx
@@ -0,0 +1,132 @@
+import type { Theme } from "@emotion/react";
+import { Typography, Stack, TextField, Button } from "@mui/material";
+import { Box, type SxProps } from "@mui/system";
+import Logo from "../../../assets/Logo.png";
+import { useState } from "react";
+
+/**Typography
+ * pH1 H1 H2 H3 H4 H5 H6
+ */
+/** Stack
+ * div que tiene por defecto display flex
+ * direction: row | column
+ * gap: espacio entre los elementos
+ * bgcolor: color de fondo
+ * height: altura del stack
+ * width: ancho del stack
+ * spacing: espacio entre los elementos
+ * padding: espacio dentro del stack
+ * margin: espacio fuera del stack
+ * alignItems: alinear los elementos dentro del stack
+ * justifyContent: justificar los elementos dentro del stack
+ * flexDirection: dirección del stack
+ * flexWrap: envolver los elementos dentro del stack
+ * flexGrow: crecer el stack
+ * #0D253F
+ * como componentes
+ */
+const stylesInputs: SxProps<Theme> = {
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderColor: "#fff",
+    },
+    "&:hover fieldset": {
+      borderColor: "#fff",
+    },
+    "&.Mui-focused fieldset": {
+      borderColor: "#fff",
+    },
+    color: "#fff",
+  },
+  "& .MuiInputLabel-root": {
+    color: "#fff",
+    "&.Mui-focused": {
+      color: "#fff",
+    },
+  },
+  "& .MuiInputBase-input": {
+    color: "#fff",
+  },
+};
+
+export const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "dexiblue.desing@gmail.com",
+    password: "",
+  });
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    //const formData = new FormData(e.currentTarget);
+    alert("test")
+    
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {value, name} = e.target;
+    setFormData({
+      ...formData,
+      [name] : value,
+      
+    });
+    
+  }
+  
+  return (
+    <Stack
+      bgcolor="#3a4348"
+      height="100vh"
+      direction="row"
+      component="main"
+      gap={2}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      <Stack
+        bgcolor="#0D253F"
+        maxHeight="50%"
+        gap={2}
+        p={5}
+        borderRadius={"16px"}
+        component="form"
+        onSubmit={handleSubmit}
+      >
+        
+        <Box component ="img" src={Logo} alt="Logo" />
+        <Typography
+          component="h3"
+          variant="h1"
+          color="#fff"
+          fontWeight={700}
+          textAlign={"center"}
+         
+        >
+          Movie
+        </Typography>
+        <TextField
+          label="Email"
+          variant="outlined"
+          type="email"
+          sx={stylesInputs}
+           name="email"
+           value={formData.email}
+           onChange={handleChange}      />
+        
+        <TextField
+          label="Password"
+          variant="outlined"
+          type="Password"
+          sx={stylesInputs}
+           name="password"
+           value={formData.password}
+           onChange={handleChange}
+        />
+
+        <Button variant="contained" type="submit">
+          Sing In
+        </Button>
+      </Stack>
+    </Stack>
+  );
+};
+
+
